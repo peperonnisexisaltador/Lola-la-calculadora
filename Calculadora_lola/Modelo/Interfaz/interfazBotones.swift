c98274953c1c915e6490c5fc2321418129491fcf
alumno <alumno@iMac-V108-05.local>
2024-09-11 10:33:21 -0600

    [Septiembre 11]

--- Calculadora_lola/Modelo/Interfaz/interfazBotones.swift
+++ Calculadora_lola/Modelo/Interfaz/interfazBotones.swift
@@ -6,83 +6,66 @@
 //
 
 import Foundation
+import UIKit
 
 //Interfaz usuario [Nombre de la clase on estructura]
-struct IUBotonCalculadora{
-    var restorationID: String
+struct IUBotonCalculadora {
+    var referencia_a_boton_interfaz:UIButton?
     var numero: Character
     var operacion: String
-    init(_ id: String, numero: Character, operacion: String) {
-        self.restorationID = id
+    
+    init(numero: Character, operacion: String) {
+        self.referencia_a_boton_interfaz = nil
         self.numero = numero
         self.operacion = operacion
     }
-    static func crear_arreglo() -> Dictionary<String, IUBotonCalculadora>{
-        var botones_interfaz: Dictionary <String, IUBotonCalculadora> = [:]
+    
+    static func crear_arreglo_botones() -> Dictionary<String, IUBotonCalculadora> {
+        var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
+        
 
+        botones_interfaz["boton_0"] = IUBotonCalculadora(
+                                                    numero: Character("0"),
+                                                    operacion: ""
+                                                    )
         
-        
-        botones_interfaz ["boton_0"] = IUBotonCalculadora (
-                                        "boton0",
-                                        numero: Character ("0"),
-                                        operacion: ""
-                                        )
-        
-        botones_interfaz ["boton_1"] = IUBotonCalculadora (
-                                        "boton1",
-                                        numero: Character ("1"),
-                                        operacion: "+"
-                                        )
-        
-        botones_interfaz ["boton_2"] = IUBotonCalculadora (
-                                        "boton2",
-                                        numero: Character ("2"),
-                                        operacion: ""
-                                        )
-        
-        botones_interfaz ["boton_3"] = IUBotonCalculadora (
-                                        "boton3",
-                                        numero: Character ("3"),
-                                        operacion: "-"
-                                        )
-        
-        botones_interfaz ["boton_4"] = IUBotonCalculadora (
-                                        "boton4",
-                                        numero: Character ("4"),
-                                        operacion: ""
-                                        )
-        
-        botones_interfaz ["boton_5"] = IUBotonCalculadora (
-                                        "boton5",
-                                        numero: Character ("5"),
-                                        operacion: ""
-                                        )
-        
-        botones_interfaz ["boton_6"] = IUBotonCalculadora (
-                                        "boton6",
-                                        numero: Character ("6"),
-                                        operacion: ""
-                                        )
-        
-        botones_interfaz ["boton_7"] = IUBotonCalculadora (
-                                        "boton7",
-                                        numero: Character ("7"),
-                                        operacion: "*"
-                                        )
-        
-        botones_interfaz ["boton_8"] = IUBotonCalculadora (
-                                        "boton8",
-                                        numero: Character ("8"),
-                                        operacion: ""
-                                        )
-        
-        botones_interfaz ["boton_9"] = IUBotonCalculadora (
-                                        "boton9",
-                                        numero: Character ("9"),
-                                        operacion: "/"
-                                        )
+        botones_interfaz["boton_1"] = IUBotonCalculadora(
+                                                    numero: Character("1"),
+                                                    operacion: "+"
+                                                    )
+        botones_interfaz["boton_2"] = IUBotonCalculadora(
+                                                    numero: Character("2"),
+                                                    operacion: ""
+                                                    )
+        botones_interfaz["boton_3"] = IUBotonCalculadora(
+                                                    numero: Character("3"),
+                                                    operacion: "-"
+                                                    )
+        botones_interfaz["boton_4"] = IUBotonCalculadora(
+                                                    numero: Character("4"),
+                                                    operacion: ""
+                                                    )
+        botones_interfaz["boton_5"] = IUBotonCalculadora(
+                                                    numero: Character("5"),
+                                                    operacion: ""
+                                                    )
+        botones_interfaz["boton_6"] = IUBotonCalculadora(
+                                                    numero: Character("6"),
+                                                    operacion: ""
+                                                    )
+        botones_interfaz["boton_7"] = IUBotonCalculadora(
+                                                    numero: Character("7"),
+                                                    operacion: "*"
+                                                    )
+        botones_interfaz["boton_8"] = IUBotonCalculadora(
+                                                    numero: Character("8"),
+                                                    operacion: ""
+                                                    )
+        botones_interfaz["boton_9"] = IUBotonCalculadora(
+                                                    numero: Character("9"),
+                                                    operacion: "/"
+                                                    )
         
         return botones_interfaz
     }
 }
-
--- Calculadora_lola/ViewController.swift
+++ Calculadora_lola/ViewController.swift
@@ -17,12 +17,16 @@ class ViewController: UIViewController {
     estados_de_la_calculadora.seleccionar_numeros
     
     @IBOutlet weak var texto_a_cambiar:UILabel!
-    @IBOutlet weak var boton_operacion:UILabel!
-    
     var botones_interffaz: Dictionary<String, IUBotonCalculadora> = [:]
     var operacion_actual: String? = nil
+    
+    @IBOutlet weak var boton_operacion:UILabel!
+    @IBOutlet weak var vista_stack:UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
        inicializar_calculadora()
     }
 
@@ -38,6 +42,7 @@ class ViewController: UIViewController {
             else {
                 operacion_actual = nil
             }
+            dibujar_numeros_u_operaciones_en_interfaz()
         }
        
         
@@ -50,21 +55,39 @@ class ViewController: UIViewController {
     }
     func inicializar_calculadora () -> Void{
         crear_arreglo_botones()
+        identificar_botones()
     }
     func crear_arreglo_botones () -> Void {
        
-        botones_interffaz = IUBotonCalculadora.crear_arreglo()
+        botones_interffaz = IUBotonCalculadora.crear_arreglo_botones()
         
     }
     func dibujar_numeros_u_operaciones_en_interfaz() {
         if(estado_actual == estados_de_la_calculadora.escoger_operacion){
-            
+            for elemento in botones_interffaz.values{
+                elemento.referencia_a_boton_interfaz?.setTitle("ñ", for: .normal)
         }
         else if (estado_actual == estados_de_la_calculadora.seleccionar_numeros){
-            
+            for elemento in botones_interffaz.values{
+                elemento.referencia_a_boton_interfaz?.setTitle(String(elemento.numero), for: .normal)
         }
     }
-    
+    func identificar_botones(){
+        for pila_de_componentes in vista_stack.subviews{
+            for boton in pila_de_componentes.subviews{
+                
+                if let identificador = boton.restorationIdentifier{
+                    print(botones_interffaz[identificador]?.referencia_a_boton_interfaz = boton as? UIButton)
+                }
+                
+                for elemento in botones_interffaz.values{
+                    elemento.referencia_a_boton_interfaz?.setTitle("ñ", for: .normal)
+                    
+                }
+                
+            }
+        }
+    }
     
 }
 
